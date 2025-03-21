75c97abda53350f6cce0c95fa1245d64b3004668
Gabriel Vidrine <vidrinegabriel@gmail.com>
2025-03-21 09:29:47 +0000

    Add settings front end page

--- KnowledgeBase.Client/knowledgebase.client.client/src/app/app.module.ts
+++ KnowledgeBase.Client/knowledgebase.client.client/src/app/app.module.ts
@@ -7,7 +7,7 @@ import { HomePageComponent } from './pages/home-page/home-page.component';
 import {ArticleViewComponent} from './pages/article-view/article-view.component';
 import { ArticleEditorComponent } from './pages/article-editor/article-editor.component';
 import { ArticleCreateComponent } from './pages/article-create/article-create.component';
-//import {SettingsPageComponent} from '.pages/settings-page/settings-page.component';
+import {SettingsPageComponent} from './pages/settings-page/settings-page.component';
 import { HttpClientModule } from '@angular/common/http';
 
 
@@ -17,8 +17,8 @@ const routes: Routes = [
   { path: '', component: HomePageComponent },
   { path: 'article/:id', component: ArticleViewComponent },
   { path: 'edit/:id', component: ArticleEditorComponent },
-  { path: 'create', component: ArticleCreateComponent }
-  //{ path: 'settings', component: SettingsPageComponent }
+  { path: 'create', component: ArticleCreateComponent },
+  { path: 'settings', component: SettingsPageComponent }
 ];
 
 
@@ -31,6 +31,7 @@ const routes: Routes = [
     ArticleEditorComponent,
     ArticleEditorComponent,
     ArticleCreateComponent,
+    SettingsPageComponent,
    // SettingsPageComponent
   ],
   imports: [
